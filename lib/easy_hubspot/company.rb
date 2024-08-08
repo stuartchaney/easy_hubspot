3cd13465a2ced89af42e5775b29c26f73bb6478a
stuartchaney <stuartchaney22@gmail.com>
2024-08-08 12:19:16 -0700

    Add company and property resources

---
+++ lib/easy_hubspot/company.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module EasyHubspot
+  # class EasyHubspot::Company
+  class Company < EasyHubspot::Base
+    class << self
+      COMPANY_ENDPOINT = 'crm/v3/objects/companies'
+
+      def get_company(company_id, property_list = [])
+        Client.do_get(company_id_endpoint(company_id, property_list), headers)
+      end
+
+      def get_companies(property_list = [], after = nil)
+        property_list = get_all_properties if property_list.empty?
+        url = COMPANY_ENDPOINT + "?limit=100&properties=#{CGI.escape(property_list.join(','))}"
+        url += "&after=#{after}" if after
+        Client.do_get(url, headers)
+      end
+
+      def create_company(body)
+        Client.do_post(COMPANY_ENDPOINT, body, headers)
+      end
+
+      def update_company(company_id, body)
+        Client.do_patch(company_id_endpoint(company_id), body, headers)
+      end
+
+      def delete_company(company_id)
+        Client.do_delete(company_id_endpoint(company_id), headers)
+      end
+
+      def update_or_create_company(email, body)
+        res = get_company(email)
+        if res && res[:id]
+          update_company(email, body)
+        else
+          create_company(body)
+        end
+      end
+
+      def get_all_properties
+        @get_all_properties ||= EasyHubspot::Property.get_properties("companies")[:results].collect { |property| property[:name] }
+      end
+
+      private
+
+      def company_id_endpoint(company_id, property_list)
+        property_list = get_all_properties if property_list.empty?
+        url = "#{COMPANY_ENDPOINT}/#{company_id}"
+        url += "?properties=#{CGI.escape(property_list.join(','))}"
+        return url
+      end
+    end
+  end
+end
---
+++ lib/easy_hubspot/property.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module EasyHubspot
+  # class EasyHubspot::Company
+  class Property < EasyHubspot::Base
+    class << self
+      PROPERTY_ENDPOINT = 'crm/v3/properties/'
+
+      def get_property(object_type, property_name)
+        Client.do_get(property_name_endpoint(object_type, property_name), headers)
+      end
+
+      def get_properties(object_type)
+        Client.do_get(properties_endpoint(object_type), headers)
+      end
+
+      private
+
+      def properties_endpoint(object_type)
+        "#{PROPERTY_ENDPOINT}/#{object_type}"
+      end
+
+      def property_id_endpoint(object_type, property_id)
+        url = "#{PROPERTY_ENDPOINT}/#{company_id}"
+        url += "?properties=#{CGI.escape(property_list.join(','))}" if property_list
+        return url
+      end
+    end
+  end
+end
